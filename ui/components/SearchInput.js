bff617b668a372f9fa7fed51b35cafe8da83fab0
Matthew Doll <matthew.doll@crtkl.com>
2018-11-06 18:16:22 -0600

    Refactor into Modules

---
+++ ui/components/SearchInput.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Icon from './Icon';
+import Styles from './StyledSearchInput';
+
+const SearchInput = ({ placeholder, value, onChange }) => (
+  <Styles.SearchInput className="SearchInput">
+    <Icon iconStyle="solid" icon="search" />
+    <input
+      type="text"
+      name="search"
+      className="form-control"
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+    />
+  </Styles.SearchInput>
+);
+
+SearchInput.defaultProps = {
+  placeholder: 'Search...',
+};
+
+SearchInput.propTypes = {
+  placeholder: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default SearchInput;
