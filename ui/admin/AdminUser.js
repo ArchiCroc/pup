4a3cfee2e81b37ec93155aa064d9778dceac9e56
Matthew Doll <matthew.doll@callisonrtkl.com>
2019-05-29 00:18:35 -0500

    converted AdminUser container to antd

--- ui/admin/AdminUser.js
+++ ui/admin/AdminUser.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { compose, graphql } from 'react-apollo';
-import { Breadcrumb, Tab } from 'react-bootstrap';
+
+import Tabs from 'antd/lib/tabs';
+import Breadcrumb from 'antd/lib/breadcrumb';
+
+// import { Breadcrumb, Tab } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import message from 'antd/lib/message';
 import AdminUserProfile from './components/AdminUserProfile';
@@ -14,63 +18,60 @@ import {
 
 import Styles from './StyledAdminUser';
 
-class AdminUser extends React.Component {
-  state = { activeTab: 'profile' };
+const AdminUser = (props) => {
+  const { data, updateUser, removeUser } = props;
+  const name = data.user && data.user.name;
+  const username = data.user && data.user.username;
 
-  render() {
-    const { data, updateUser, removeUser } = this.props;
-    const name = data.user && data.user.name;
-    const username = data.user && data.user.username;
-
-    return data.user ? (
-      <div className="AdminUser">
-        <Breadcrumb>
-          <li>
-            <Link to="/admin/users">Users</Link>
-          </li>
-          <Breadcrumb.Item active>{name ? `${name.first} ${name.last}` : username}</Breadcrumb.Item>
-        </Breadcrumb>
-        <Styles.AdminUserHeader className="page-header">
-          {name ? `${name.first} ${name.last}` : username}
-          {data.user.oAuthProvider && (
-            <span className={`label label-${data.user.oAuthProvider}`}>
-              {data.user.oAuthProvider}
-            </span>
-          )}
-        </Styles.AdminUserHeader>
-        <Styles.AdminUserTabs
+  return data.user ? (
+    <div className="AdminUser">
+      <Breadcrumb>
+        <Breadcrumb.Item>
+          <Link to="/admin/users">Users</Link>
+        </Breadcrumb.Item>
+        <Breadcrumb.Item>{name ? `${name.first} ${name.last}` : username}</Breadcrumb.Item>
+      </Breadcrumb>
+      <Styles.AdminUserHeader className="page-header">
+        {name ? `${name.first} ${name.last}` : username}
+        {data.user.oAuthProvider && (
+          <span className={`label label-${data.user.oAuthProvider}`}>
+            {data.user.oAuthProvider}
+          </span>
+        )}
+      </Styles.AdminUserHeader>
+      {/* <Styles.AdminUserTabs
           animation={false}
           activeKey={this.state.activeTab}
           onSelect={(activeTab) => this.setState({ activeTab })}
           id="admin-user-tabs"
-        >
-          <Tab eventKey="profile" title="Profile">
-            <AdminUserProfile
-              user={data.user}
-              updateUser={(options, callback) => {
-                // NOTE: Do this to allow us to perform work inside of AdminUserProfile
-                // after a successful update. Not ideal, but hey, c'est la vie.
-                updateUser(options);
-                if (callback) callback();
-              }}
-              removeUser={removeUser}
-            />
-          </Tab>
-          <Tab eventKey="settings" title="Settings">
-            <UserSettings
-              isAdmin
-              userId={data.user._id}
-              settings={data.user.settings}
-              updateUser={updateUser}
-            />
-          </Tab>
-        </Styles.AdminUserTabs>
-      </div>
-    ) : (
-      <div />
-    );
-  }
-}
+        > */}
+      <Tabs>
+        <Tabs.TabPane key="profile" tab="Profile">
+          <AdminUserProfile
+            user={data.user}
+            updateUser={(options, callback) => {
+              // NOTE: Do this to allow us to perform work inside of AdminUserProfile
+              // after a successful update. Not ideal, but hey, c'est la vie.
+              updateUser(options);
+              if (callback) callback();
+            }}
+            removeUser={removeUser}
+          />
+        </Tabs.TabPane>
+        <Tabs.TabPane key="settings" tab="Settings">
+          <UserSettings
+            isAdmin
+            userId={data.user._id}
+            settings={data.user.settings}
+            updateUser={updateUser}
+          />
+        </Tabs.TabPane>
+      </Tabs>
+    </div>
+  ) : (
+    <div />
+  );
+};
 
 AdminUser.propTypes = {
   data: PropTypes.object.isRequired,
