2c303eb08764e98b5a48cd6d4f7d9033db2d2c85
Matthew Doll <matthew.doll@crtkl.com>
2019-10-04 13:24:36 -0500

    add all the fields to the fragment

---
+++ ui/components/FormatDate.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import moment from 'moment';
+import 'moment-timezone';
+import PropTypes from 'prop-types';
+
+const FormatDate = (props) => {
+  let timezone = props.timezone && props.timezone;
+
+  if (!timezone) {
+    timezone = moment.tz.guess();
+  }
+  const timestamp = !timezone
+    ? moment.utc(props.timestamp)
+    : moment.utc(props.timestamp).tz(timezone);
+
+  return <>{timestamp.format('MMMM Do, YYYY [at] hh:mm a')}></>;
+};
+
+FormatDate.defaultProps = {
+  timezone: undefined,
+  format: 'MMMM Do, YYYY [at] hh:mm a'm
+};
+
+FormatDate.propTypes = {
+  timestamp: PropTypes.string.isRequired,
+  timezone: PropTypes.string,
+  format: PropTypes.string,
+};
+
+export default FormatDate;
