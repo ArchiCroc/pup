bd5c011c01a0f22c62d7cd99fa9beca1c4179bb5
Matthew Doll <matthew.doll@callisonrtkl.com>
2019-10-23 02:23:26 -0500

    fix formating

--- ui/components/AuthenticatedNavigation.jsx
+++ ui/components/AuthenticatedNavigation.jsx
@@ -18,19 +18,52 @@ const AuthenticatedNavigation = ({ name, history, roles, location = { pathname: 
       {/* #### ERROR_REPORTS_USER_MENU_ITEMS_START #### */}
       {/* #### ERROR_REPORTS_USER_MENU_ITEMS_END #### */}
       {/* #### PAGES_USER_MENU_ITEMS_START #### */}
-      <Menu.Item key="/pages/example">
-        {i18n.__('Pages.example')}
-        <Link to="/pages/example" />
-      </Menu.Item>
-
       {/* #### PAGES_USER_MENU_ITEMS_END #### */}
       {/* #### USERS_USER_MENU_ITEMS_START #### */}
       {/* #### USERS_USER_MENU_ITEMS_END #### */}
       {/* #### PLOP_USER_MENU_ITEMS_END #### */}
-
       {hasRole(roles, 'admin') && (
         <Menu.SubMenu title={i18n.__('admin')}>
           {/* #### PLOP_ADMIN_MENU_ITEMS_START #### */}
-
           {/* #### PAGES_ADMIN_MENU_ITEMS_START #### */}
-          {/* #### PAGES_ADMIN_MENU_ITEMS_END #### */}          {/* #### ERROR_REPORTS_ADMIN_MENU_ITEMS_START #### */}          <Menu.Item key="/error-reports">            {i18n.__('ErrorReports.error_report_plural')}            <Link to="/error-reports" />          </Menu.Item>          {/* #### ERROR_REPORTS_ADMIN_MENU_ITEMS_END #### */}          {/* #### USERS_ADMIN_MENU_ITEMS_START #### */}          <Menu.Item key="/admin/users">            {i18n.__('Users.user_plural')}            <Link to="/admin/users" />          </Menu.Item>          {/* #### USERS_ADMIN_MENU_ITEMS_END #### */}          {/* #### PLOP_ADMIN_MENU_ITEMS_END #### */}        </Menu.SubMenu>      )}    </Menu>    <Menu      theme="dark"      mode="horizontal"      defaultSelectedKeys={[location.pathname]}      style={{ float: 'right', lineHeight: '64px' }}      data-test="user-nav-dropdown"    >      <Menu.SubMenu title={name}>        <Menu.Item key="/user/profile">          {i18n.__('Users.profile')}          <Link to="/user/profile" />        </Menu.Item>        <Menu.Item key="/logout">          {i18n.__('Users.logout')}          <Link to="/logout" />        </Menu.Item>      </Menu.SubMenu>    </Menu>  </>);AuthenticatedNavigation.propTypes = {  name: PropTypes.string.isRequired,  history: PropTypes.object.isRequired,  userId: PropTypes.string.isRequired,  location: PropTypes.object.isRequired,  roles: PropTypes.array.isRequired,};export default withRouter(AuthenticatedNavigation);
+          {/* #### PAGES_ADMIN_MENU_ITEMS_END #### */}
+          {/* #### ERROR_REPORTS_ADMIN_MENU_ITEMS_START #### */}
+          <Menu.Item key="/error-reports">
+            {i18n.__('ErrorReports.error_report_plural')} <Link to="/error-reports" />
+          </Menu.Item>
+          {/* #### ERROR_REPORTS_ADMIN_MENU_ITEMS_END #### */}
+          {/* #### USERS_ADMIN_MENU_ITEMS_START #### */}
+          <Menu.Item key="/admin/users">
+            {i18n.__('Users.user_plural')} <Link to="/admin/users" />
+          </Menu.Item>
+          {/* #### USERS_ADMIN_MENU_ITEMS_END #### */}
+          {/* #### PLOP_ADMIN_MENU_ITEMS_END #### */}
+        </Menu.SubMenu>
+      )}
+    </Menu>
+    <Menu
+      theme="dark"
+      mode="horizontal"
+      defaultSelectedKeys={[location.pathname]}
+      style={{ float: 'right', lineHeight: '64px' }}
+      data-test="user-nav-dropdown"
+    >
+      <Menu.SubMenu title={name}>
+        <Menu.Item key="/user/profile">
+          {i18n.__('Users.profile')} <Link to="/user/profile" />
+        </Menu.Item>
+        <Menu.Item key="/logout">
+          {i18n.__('Users.logout')} <Link to="/logout" />
+        </Menu.Item>
+      </Menu.SubMenu>
+    </Menu>
+  </>
+);
+AuthenticatedNavigation.propTypes = {
+  name: PropTypes.string.isRequired,
+  history: PropTypes.object.isRequired,
+  userId: PropTypes.string.isRequired,
+  location: PropTypes.object.isRequired,
+  roles: PropTypes.array.isRequired,
+};
+export default withRouter(AuthenticatedNavigation);
