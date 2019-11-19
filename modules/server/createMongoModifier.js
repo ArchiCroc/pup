dc97d8987f525e788f7541ae0535a874202decde
Matthew Doll <matthew.doll@callisonrtkl.com>
2019-11-19 01:04:30 -0600

    update createMongo Modifier to diff vs current object

--- modules/server/createMongoModifier.js
+++ modules/server/createMongoModifier.js
@@ -1,22 +1,28 @@
 import difference from 'lodash/difference';
+import convertToDotKeys from './convertToDotKeys';
 
-function createMongoModifier(schema, object) {
-  const keys = Object.keys(schema.schema());
+const typesToSkip = ['object'];
 
-  const unsetKeys = difference(keys, Object.keys(object));
+function createMongoModifier(currentObject, newObject) {
+  currentObject = convertToDotKeys(currentObject);
+  newObject = convertToDotKeys(newObject);
+  const unsetKeys = difference(Object.keys(currentObject), Object.keys(newObject));
   const $unset = {};
 
   for (let i = 0; i < unsetKeys.length; i++) {
     const key = unsetKeys[i];
+
     // @todo this needs more testing
-    if (!key.includes('$')) {
-      $unset[key] = 1;
-    }
+    //const { type } = currentSchema[key].type.definitions[0];
+    //console.log('type', key, typeof type, type);
+    // if (typesToSkip.includes(typeof type)) {
+    $unset[key] = 1;
+    // }
   }
 
   const modifiers = {};
-  if (Object.keys(object).length > 0) {
-    modifiers.$set = object;
+  if (Object.keys(newObject).length > 0) {
+    modifiers.$set = newObject;
   }
   if (Object.keys($unset).length > 0) {
     modifiers.$unset = $unset;
